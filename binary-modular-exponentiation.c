ab0ddc9244e154417e1e01f0c3109c32b7fbaaff
Michael Canonizado <michaelxaviercanonizado@gmail.com>
2024-01-29 19:26:16 +0800

    feat: add decimal to binary conversion using unsigned long long

--- binary-modular-exponentiation.c
+++ binary-modular-exponentiation.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <math.h>
 
+unsigned long long decimalToBinary(unsigned long long num);
+
 int main(void) {
     // Example of tried keys and n:
     // e (Public Key) : 3001
@@ -8,9 +10,29 @@ int main(void) {
     // n (P * Q) : 421999
     // Encrypt ex. : H (72) -> 351180
 
-    unsigned long long int exponent = 3001;
-    unsigned long long int base = 72;
-    unsigned int divisor = 421999;
+    // long long exponent = 13;
+    unsigned long long int exponent = 300121;
+    long long int base = 72;
+    long long int divisor = 421999;
+
+    unsigned long long res = decimalToBinary(exponent);
+
+    printf("\n%llu -> %llu\n\n", exponent, res);
 
     return 0;
+}
+
+unsigned long long decimalToBinary(unsigned long long num) {
+    int remainder;
+    unsigned long long result = 0;
+    unsigned long long i = 1;
+
+    while (num != 0) {
+        remainder = num % 2;
+        num /= 2;
+        result += remainder * i;
+        i *= 10;
+    }
+
+    return result;
 }
