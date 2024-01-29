26235b9c3fc02c5e760c13287fc09894ec9c9447
Michael Canonizado <michaelxaviercanonizado@gmail.com>
2024-01-29 18:24:04 +0800

    feat: add test values

--- binary-modular-exponentiation.c
+++ binary-modular-exponentiation.c
@@ -2,7 +2,15 @@
 #include <math.h>
 
 int main(void) {
-    printf("Hello World");
+    // Example of tried keys and n:
+    // e (Public Key) : 3001
+    // d (Private Key) : 841
+    // n (P * Q) : 421999
+    // Encrypt ex. : H (72) -> 351180
+
+    unsigned long long int exponent = 3001;
+    unsigned long long int base = 72;
+    unsigned int divisor = 421999;
 
     return 0;
 }
