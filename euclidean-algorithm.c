acb38eb4ae0799ef0070811a0f2f02f725e3fa58
Michael Canonizado <michaelxaviercanonizado@gmail.com>
2024-01-30 18:13:09 +0800

    docs: add comments

--- euclidean-algorithm.c
+++ euclidean-algorithm.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
 
 unsigned long long int gcd(unsigned long long int  num1, unsigned long long int num2) {
+    // Uses the Euclidean Algorithm to get the GCD of 2 numbers quickly (ref: https://www.youtube.com/watch?v=yHwneN6zJmU&t=695s)
     unsigned long long int quotient, remainder;
 
+    // Find bigger number between the two numbers
     unsigned long long int a = num1 > num2 ? num1 : num2;
     unsigned long long int b = num1 < num2 ? num1 : num2;
 
+    // Repeat until b becomes zero, then return a
     while(b > 0) {
+        // Get the remainder and quotient
         remainder = a % b;
         quotient = a / b;
 
+        // Shift numbers
         a = b;
         b = remainder;
     }
