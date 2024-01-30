12e28efdb333108de84f1ea46aaf17ee808e443a
Michael Canonizado <michaelxaviercanonizado@gmail.com>
2024-01-30 19:49:58 +0800

    feat: add loop count tracker to tracker loop cycles to calculate GCD and mod inverse

--- euclidean-algorithm.c
+++ euclidean-algorithm.c
@@ -4,6 +4,8 @@ unsigned long long int gcd(unsigned long long int  num1, unsigned long long int 
     // Uses the Euclidean Algorithm to get the GCD of 2 numbers quickly (ref: https://www.youtube.com/watch?v=yHwneN6zJmU&t=695s)
     unsigned long long int quotient, remainder;
 
+    unsigned int count = 0;
+
     // Find bigger number between the two numbers
     unsigned long long int a = num1 > num2 ? num1 : num2;
     unsigned long long int b = num1 < num2 ? num1 : num2;
@@ -17,7 +19,11 @@ unsigned long long int gcd(unsigned long long int  num1, unsigned long long int 
         // Shift numbers
         a = b;
         b = remainder;
+
+        count++;
     }
+
+    printf("\nFinding GCD took #%u rounds.", count);
 
     return a;
 }
@@ -28,6 +34,8 @@ unsigned long long int modInverse(unsigned long long int  num, unsigned long lon
     long long int t;
     long long int t1 = 0;
     long long int t2 = 1;
+
+    unsigned int count = 0;
 
     // Find bigger number between the two numbers
     unsigned long long int a = num > divisor ? num : divisor;
@@ -45,7 +53,11 @@ unsigned long long int modInverse(unsigned long long int  num, unsigned long lon
         b = remainder;
         t1 = t2;
         t2 = t;
+
+        count++;
     }
+
+    printf("\nFinding Mod Inverse took #%u rounds.", count);
 
     // Check if t1 is the negative mod inverse. If true, return the positive inverse by offsetting by the divisor
     if (t1 < 0) {
@@ -62,7 +74,7 @@ int main (void) {
     unsigned long long int gcdResult = gcd(num1,divisor);
     unsigned long long int modInverseResult = modInverse(num1,divisor);
 
-    printf("\nGCD of %llu and %llu is %llu\n", num1, divisor, gcdResult);
+    printf("\n\nGCD of %llu and %llu is %llu", num1, divisor, gcdResult);
 
     printf("\nMod Inverse of %llu mod %llu is %lld\n\n", num1, divisor, modInverseResult);
 
