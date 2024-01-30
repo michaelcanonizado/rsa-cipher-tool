55e8aace3a030424edddfd469fcc133519d1b5b5
Michael Canonizado <michaelxaviercanonizado@gmail.com>
2024-01-30 19:41:36 +0800

    feat: add extended euclidean algorithm code to find mod inverse of 2 numbers

--- euclidean-algorithm.c
+++ euclidean-algorithm.c
@@ -22,13 +22,43 @@ unsigned long long int gcd(unsigned long long int  num1, unsigned long long int 
     return a;
 }
 
+unsigned long long int modInverse(unsigned long long int  num, unsigned long long int divisor) {
+    unsigned long long int quotient, remainder;
+    long long int t;
+    long long int t1 = 0;
+    long long int t2 = 1;
+
+    unsigned long long int a = num > divisor ? num : divisor;
+    unsigned long long int b = num < divisor ? num : divisor;
+
+    while(b > 0) {
+        remainder = a % b;
+        quotient = a / b;
+        t = t1 - (t2 * quotient);
+
+        a = b;
+        b = remainder;
+        t1 = t2;
+        t2 = t;
+    }
+
+    if (t1 < 0) {
+        return t1 + divisor;
+    } else {
+        return t1;
+    }
+}
+
 int main (void) {
-    unsigned long long int num1 = 9811;
-    unsigned long long int num2 = 31427928;
+    unsigned long long int num1 = 24481;
+    unsigned long long int divisor = 30783412;
 
-    unsigned long long int result = gcd(num1,num2);
+    unsigned long long int gcdResult = gcd(num1,divisor);
+    unsigned long long int modInverseResult = modInverse(num1,divisor);
 
-    printf("\nGCD of %llu and %llu is %llu\n\n", num1, num2, result);
+    printf("\nGCD of %llu and %llu is %llu\n", num1, divisor, gcdResult);
+
+    printf("\nMod Inverse of %llu mod %llu is %lld\n\n", num1, divisor, modInverseResult);
 
     return 0;
 }
