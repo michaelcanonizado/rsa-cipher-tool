f86f782165cabbe7af8e61237288a4f4bc5c4c36
Michael Canonizado <michaelxaviercanonizado@gmail.com>
2024-01-31 20:42:01 +0800

    feat: add Bignum struct

--- bignum.c
+++ bignum.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+#define MAX_INT_LENGTH 10000
+
+typedef struct {
+    int digits[MAX_INT_LENGTH];
+    int length;
+} Bignum;
+
 int main(void) {
     printf("Hello World");
 
