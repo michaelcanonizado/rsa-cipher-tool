35157a0b1811f1b658cf332858f22c768671080c
unknown <michaelxaviercanonizado@gmail.com>
2024-02-05 11:29:39 +0800

    refactor: change count increment in intToBignum() from count++; to count + 1;

--- bignum.c
+++ bignum.c
@@ -42,7 +42,7 @@ void intToBignum(Bignum *numStruct, unsigned long long int integer) {
     }
 
     // Store the count in Bignum.length
-    numStruct->length = count++;
+    numStruct->length = count + 1;
 }
 
 void addBignum(Bignum *result, Bignum *num1, Bignum *num2) {
