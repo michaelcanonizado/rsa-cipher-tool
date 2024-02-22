54dc27f287109294dd8380f1bcc2fb5f27ca3ff5
Michael Canonizado <michaelxaviercanonizado@gmail.com>
2024-02-22 22:16:13 +0800

    test(bignum-to-int): add test output format and test bignumToInt

--- tests/unit/bignum-to-int.c
+++ tests/unit/bignum-to-int.c
@@ -1,29 +1,59 @@
 #include <stdio.h>
 #include <math.h>
+#include <string.h>
 #include "../../bignum.h"
 
 int main(void) {
-    printf("\n\n--------------------------------------------------------------------------------------");
-    printf("\n| %27s            || %27s              |", "GENERATED BIGNUM", "BIGNUM TO INT");
-    printf("\n--------------------------------------------------------------------------------------");
-    printf("\n| %5s  | %6s | %17s    || %18s     | %6s | %5s  |", "SIGN", "LENGTH", "BIGNUM STRUCT", "LONG LONG INT", "LENGTH", "SIGN");
-    printf("\n--------------------------------------------------------------------------------------");
+    int errorCounter = 0;
 
-    Bignum num = initBignum();
-    
-    intToBignum(&num, 92, negative);
+    printf("\n\n-------------------------------------------------------------------------------------------------------------");
+    printf("\n| %27s            || %27s              || %13s       |", "GENERATED BIGNUM", "BIGNUM TO INT", "STATUS");
+    printf("\n-------------------------------------------------------------------------------------------------------------");
+    printf("\n| %5s  | %6s | %17s    || %18s     | %6s | %5s  || %7s  | %6s   |", "SIGN", "LENGTH", "BIGNUM DIGITS", "LONG LONG INT", "LENGTH", "SIGN", "LENGTH", "SIGN");
+    printf("\n-------------------------------------------------------------------------------------------------------------");
 
-    printf("\n| %5s  | %6d | ", num.sign == positive ? "POS" : "NEG", num.length);
-    for (int i = num.length; i < 20; i++) {
-        printf(" ");
+    for (int i = -100; i <= 100; i++) {
+        Bignum num = initBignum();
+        
+        if (i < 0) {
+            intToBignum(&num, i * -1, negative);
+        } else {
+            intToBignum(&num, i, positive);
+        }
+        
+        long long int result = bignumToInt(&num);
+
+        char signStatus[10] = "MATCH";
+        char lengthStatus[10] = "MATCH";
+
+        if (
+            (num.sign == positive && result < 0) || 
+            (num.sign == negative && result >= 0)
+            ) {
+            strcpy(signStatus, "MISMATCH");
+            errorCounter++;
+        }
+
+        if (num.length != getNumOfDigitsOfInteger(result)) {
+            strcpy(lengthStatus, "MISATCH");
+            errorCounter++;
+        }
+        
+
+        printf("\n| %5s  | %6d | ", num.sign == positive ? "POS" : "NEG", num.length);
+        for (int i = num.length; i < 20; i++) {
+            printf(" ");
+        }
+        for (int i = num.length - 1; i >= 0 ; i--) {
+            printf("%d", num.digits[i]);
+        }
+
+        printf(" ||   %20lld | %6d | %5s  || %8s | %8s |", result, getNumOfDigitsOfInteger(result), result < 0 ? "NEG" : "POS", lengthStatus, signStatus);
     }
-    for (int i = num.length - 1; i >= 0 ; i--) {
-        printf("%d", num.digits[i]);
-    }
 
-    long long int result = bignumToInt(&num);
-    printf(" ||   %20lld | %6d | %5s  |", result, getNumOfDigitsOfInteger(result), result > 0 ? "POS" : "NEG");
-
+    printf("\n-------------------------------------------------------------------------------------------------------------");
+    printf("\n| %48d %-56s |", errorCounter, "ERRORS FOUND");
+    printf("\n-------------------------------------------------------------------------------------------------------------");
 
     printf("\n\n\n");
 
