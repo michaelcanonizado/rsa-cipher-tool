132ae6329e82238d0ca08f78fa866e49c1cf0ca2
Michael Canonizado <michaelxaviercanonizado@gmail.com>
2024-02-26 23:40:21 +0800

    test(bignum-addition): test bignum addition with 0

--- tests/unit/bignum-addition.c
+++ tests/unit/bignum-addition.c
@@ -60,12 +60,103 @@ void testSign(BIGNUM_SIGN integer1Sign, BIGNUM_SIGN integer2Sign, int numOfInter
     printf("\n");
 }
 
+void testZero(int numOfInterations) {
+    srand(time(NULL));
+
+    for (int i = 0; i < numOfInterations; i++) {
+
+        long long int integer1, integer2;
+    
+        integer1 = rand() % 19999 - 9999;
+        integer2 = 0;
+
+        long long int integerResult = integer1 + integer2;
+
+        Bignum bignum1 = initBignum(); 
+        Bignum bignum2 = initBignum(); 
+        Bignum bignumResult = initBignum();
+
+        if (integer1 < 0){
+            intToBignum(&bignum1, integer1 * -1, negative);
+        } else {
+            intToBignum(&bignum1, integer1, positive);
+        } 
+    
+        if (integer2 < 0) {   
+            intToBignum(&bignum2, integer2 * -1, negative);
+        } else {
+            intToBignum(&bignum2, integer2, positive);
+        }
+
+        addBignum(&bignumResult, &bignum1, &bignum2);
+
+        printBignumCenter(&bignum1, 10);
+        printf("+");
+        printBignumCenter(&bignum2, 10);
+        printf("=");
+        printBignumCenter(&bignumResult, 10);
+        printf("~ %lld | ", integerResult);
+
+        if (integerResult == bignumToInt(&bignumResult)) {
+            printf("MATCH");
+        } else {
+            printf("MISMATCH");
+        }
+        printf("\n");
+    }
+        printf("\n");
+
+    for (int i = 0; i < numOfInterations; i++) {
+        long long int integer1, integer2;
+    
+        integer1 = 0;
+        integer2 = rand() % 19999 - 9999;
+
+        long long int integerResult = integer1 + integer2;
+
+        Bignum bignum1 = initBignum(); 
+        Bignum bignum2 = initBignum(); 
+        Bignum bignumResult = initBignum();
+
+        if (integer1 < 0){
+            intToBignum(&bignum1, integer1 * -1, negative);
+        } else {
+            intToBignum(&bignum1, integer1, positive);
+        } 
+    
+        if (integer2 < 0) {   
+            intToBignum(&bignum2, integer2 * -1, negative);
+        } else {
+            intToBignum(&bignum2, integer2, positive);
+        }
+
+        addBignum(&bignumResult, &bignum1, &bignum2);
+
+        printBignumCenter(&bignum1, 10);
+        printf("+");
+        printBignumCenter(&bignum2, 10);
+        printf("=");
+        printBignumCenter(&bignumResult, 10);
+        printf("~ %lld | ", integerResult);
+
+        if (integerResult == bignumToInt(&bignumResult)) {
+            printf("MATCH");
+        } else {
+            printf("MISMATCH");
+        }
+        printf("\n");
+    }
+
+    printf("\n");
+}
+
 int main(void) {
 
     testSign(positive, positive, 5);
     testSign(negative, negative, 5);
     testSign(positive, negative, 5);
     testSign(negative, positive, 5);
+    testZero(5);
 
     return 0;
 }
