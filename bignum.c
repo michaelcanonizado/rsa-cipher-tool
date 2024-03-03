1b0f4ce22b7b4fabd64fa3343e1c6023697817a7
Michael Canonizado <michaelxaviercanonizado@gmail.com>
2024-03-03 11:07:58 +0800

    docs: add comments to printBignum()

--- bignum.c
+++ bignum.c
@@ -143,6 +143,8 @@ void copyBignum(Bignum *result, Bignum *num) {
 }
 
 void printBignum(Bignum *num) {
+    // Function to print Bignum.digits[]
+    // This won't print any \n or spaces, just the Bignum
     for (int i = num->length - 1; i >= 0; i--) {
         printf("%d", num->digits[i]);
     }
