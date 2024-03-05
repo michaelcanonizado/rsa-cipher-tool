8726b6191c706ad2fadc7df0421f7b5484e7f1ce
Michael Canonizado <michaelxaviercanonizado@gmail.com>
2024-03-05 21:13:07 +0800

    chore: initialize karatsuba function

--- karatsuba-algorithm.c
+++ karatsuba-algorithm.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 
+long long int karatsuba(int multiplicand, int multiplier) {
+    return multiplicand * multiplier;
+}
+
 int main(void) {
-    printf("Hello World!");
+    int num1 = 1234;
+    int num2 = 5678;
+
+    int result = karatsuba(num1, num2);
+
+    printf("%d * %d = %d\n", num1, num2, result);
 
     return 0;
 }
