cc78d2de7a752d252b2bb43fef1c3fa71dfbbe25
Michael Canonizado <michaelxaviercanonizado@gmail.com>
2024-03-11 20:37:30 +0800

    chore: fix indentations reorder code

--- karatsuba-algorithm.c
+++ karatsuba-algorithm.c
@@ -1,43 +1,48 @@
 #include <stdio.h>
 #include <math.h>
 
-int get_size(long);
+int get_size(long value){
+    int count = 0;
+    while (value > 0) {
+        count++;
+        value /= 10;
+    }
+    return count;
+}
+
 long karatsuba(long X, long Y){
 
-   if (X < 10 && Y < 10)
-      return X * Y;
+    if (X < 10 && Y < 10) {
+        return X * Y;
+    }
    
-   int size = fmax(get_size(X), get_size(Y));
-   if(size < 10)
-      return X * Y;
+    int size = fmax(get_size(X), get_size(Y));
+
+    if(size < 10) {
+        return X * Y;
+    }
    
-   size = (size/2) + (size%2);
+    size = (size/2) + (size%2);
 
-   long multiplier = pow(10, size);
+    long multiplier = pow(10, size);
 
-   long b = X/multiplier;
-   long a = X - (b * multiplier);
-   long d = Y / multiplier;
-   long c = Y - (d * size);
-   long ac = karatsuba(a, c);
-   long a_plus_b_times_c_plus_d = karatsuba(a + b, c + d);
-   long bd = karatsuba(b, d);
-   return ac + ((a_plus_b_times_c_plus_d - ac - bd) * multiplier) + (bd * (long)(pow(10, 2 * size)));
+    long b = X/multiplier;
+    long a = X - (b * multiplier);
+    long d = Y / multiplier;
+    long c = Y - (d * size);
+    long ac = karatsuba(a, c);
+    long a_plus_b_times_c_plus_d = karatsuba(a + b, c + d);
+    long bd = karatsuba(b, d);
+
+    return ac + ((a_plus_b_times_c_plus_d - ac - bd) * multiplier) + (bd * (long)(pow(10, 2 * size)));
 }
-int get_size(long value){
-   int count = 0;
-   while (value > 0) {
-      count++;
-      value /= 10;
-   }
-   return count;
-}
+
 int main(){
-   long x = 5678;
-   long y = 1234;
+    long x = 5678;
+    long y = 1234;
 
-   printf("\nExpected Result: %ld * %ld = %ld", x, y, x * y);
-   printf("\nKA Result: %ld * %ld = %ld\n\n", x, y, karatsuba(x, y));
+    printf("\nExpected Result: %ld * %ld = %ld", x, y, x * y);
+    printf("\nKA Result: %ld * %ld = %ld\n\n", x, y, karatsuba(x, y));
 
-   return 0;
+    return 0;
 }
