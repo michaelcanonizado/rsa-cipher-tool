41a2d79f0ab37d28a7033148663c52731ae5373d
Michael Canonizado <michaelxaviercanonizado@gmail.com>
2024-03-21 19:48:55 +0800

    fix: add semicolon after multiplyBignum() function prototype

--- bignum.h
+++ bignum.h
@@ -46,6 +46,6 @@ int isBignumZero(Bignum *num);
 
 void addBignum(Bignum *result, Bignum *num1, Bignum *num2);
 void subtractBignum(Bignum *result, Bignum *num1, Bignum *num2);
-int multiplyBignum(Bignum *result, Bignum *x, Bignum *y)
+int multiplyBignum(Bignum *result, Bignum *x, Bignum *y);
 
 #endif /* BIGNUM_H */
