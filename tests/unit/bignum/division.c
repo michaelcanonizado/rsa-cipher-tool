5e6327f1af3722e258608bbf055dd84034195cd1
deynklarys <144914287+deynklarys@users.noreply.github.com>
2024-03-28 21:17:52 +0800

    chore: add testZero

--- tests/unit/bignum/division.c
+++ tests/unit/bignum/division.c
@@ -56,6 +56,102 @@ void testSign(BIGNUM_SIGN integer1Sign, BIGNUM_SIGN integer2Sign, int numOfInter
     printf("\n");
 }
 
+void testZero(int numOfInterations) {
+    srand(time(NULL));
+
+    for (int i = 0; i < numOfInterations; i++) {
+
+        long long int integer1, integer2;
+    
+        integer1 = rand() % 19999 - 9999;
+        integer2 = 0;
+
+        long long int integerResult = 0 ;
+
+        Bignum bignum1 = initBignum(); 
+        Bignum bignum2 = initBignum(); 
+        Bignum bignumResult = initBignum();
+
+        if (integer1 < 0){
+            intToBignum(&bignum1, integer1 * -1, negative);
+        } else {
+            intToBignum(&bignum1, integer1, positive);
+        } 
+    
+        if (integer2 < 0) {   
+            intToBignum(&bignum2, integer2 * -1, negative);
+        } else {
+            intToBignum(&bignum2, integer2, positive);
+        }
+        
+        
+        divideBignum(&bignumResult, &bignum1, &bignum2);
+        if (integer2 != 0) {
+            
+            integerResult = integer1 / integer2;
+        } else {
+            integerResult = 0;
+        }
+
+        printBignumCenter(&bignum1, 10);
+        printf("/");
+        printBignumCenter(&bignum2, 10);
+        printf("=");
+        printBignumCenter(&bignumResult, 10);
+        printf("~ %10lld ", integerResult);
+
+        printf("| %s", integerResult == bignumToInt(&bignumResult) ? "MATCH" : "MISMATCH");
+
+        printf("\n");
+    }
+        printf("\n");
+
+    for (int i = 0; i < numOfInterations; i++) {
+        long long int integer1, integer2;
+    
+        integer1 = 0;
+        integer2 = rand() % 19999 - 9999;
+
+        long long int integerResult = 0;
+
+        Bignum bignum1 = initBignum(); 
+        Bignum bignum2 = initBignum(); 
+        Bignum bignumResult = initBignum();
+
+        if (integer1 < 0){
+            intToBignum(&bignum1, integer1 * -1, negative);
+        } else {
+            intToBignum(&bignum1, integer1, positive);
+        } 
+    
+        if (integer2 < 0) {   
+            intToBignum(&bignum2, integer2 * -1, negative);
+        } else {
+            intToBignum(&bignum2, integer2, positive);
+        }
+    
+        divideBignum(&bignumResult, &bignum1, &bignum2);
+        if (integer1 != 0) {
+            integerResult = integer1 / integer2;
+        } else {
+            integerResult = 0;
+        }
+
+        printBignumCenter(&bignum1, 10);
+        printf("/");
+        printBignumCenter(&bignum2, 10);
+        printf("=");
+        printBignumCenter(&bignumResult, 10);
+        printf("~ %10lld ", integerResult);
+
+        printf("| %s", integerResult == bignumToInt(&bignumResult) ? "MATCH" : "MISMATCH");
+
+        printf("\n");
+    }
+
+    printf("\n");
+}
+
 int main() {
 
     printf("\n%7s   / %7s   = %7s    ~ %10s | %s\n", "BN1", "BN2", "RESULT", "EXPECTED RESULT", "STATUS");
@@ -65,7 +161,7 @@ int main() {
     testSign(negative, negative, 5);
     testSign(positive, negative, 5);
     testSign(negative, positive, 5);
-    //testZero(5);
+    testZero(5);
     
 return 0;
 }
