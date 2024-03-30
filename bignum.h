42752c73d4d2cdc988545356ecdeecb30ca50004
Michael Canonizado <michaelxaviercanonizado@gmail.com>
2024-03-30 18:35:21 +0800

    feat: add divideBignumPrototype() function prototype

--- bignum.h
+++ bignum.h
@@ -49,6 +49,7 @@ int incrementBignum(Bignum *num);
 void addBignum(Bignum *result, Bignum *num1, Bignum *num2);
 void subtractBignum(Bignum *result, Bignum *num1, Bignum *num2);
 int multiplyBignum(Bignum *result, Bignum *multiplicand, Bignum *multiplier);
+int divideBignumPrototype(Bignum *result, Bignum *dividend, Bignum *divisor);
 int moduloBignum(Bignum *result, Bignum *dividend, Bignum *divisor);
 
 int halfBignum(Bignum *result, Bignum *num);
