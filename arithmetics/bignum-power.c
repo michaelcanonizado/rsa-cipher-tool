5170d3e0f726a79f67b98061f06407ca8139d585
Michael Canonizado <michaelxaviercanonizado@gmail.com>
2024-03-30 13:35:39 +0800

    docs: add comments to CPU runtime timer

--- arithmetics/bignum-power.c
+++ arithmetics/bignum-power.c
@@ -4,10 +4,12 @@
 #include "../bignum.h"
 
 int main(void) {
+    // Start CPU timer
     clock_t begin = clock();
 
     printf("Hello World!\n");
 
+    // End CPU timer and print elapsed time
     clock_t end = clock();
     double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
     printf("\n\nELAPSED TIME CALCULATING: %lf seconds\n\n", time_spent);
