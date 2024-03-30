d02c794fea68b17df0e833bfd792483c482019b7
Michael Canonizado <michaelxaviercanonizado@gmail.com>
2024-03-30 13:34:59 +0800

    feat: add cpu runtime timer

--- arithmetics/bignum-power.c
+++ arithmetics/bignum-power.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 #include "../bignum.h"
 
 int main(void) {
+    clock_t begin = clock();
 
     printf("Hello World!\n");
+
+    clock_t end = clock();
+    double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+    printf("\n\nELAPSED TIME CALCULATING: %lf seconds\n\n", time_spent);
 
     return 0;
 }
