aa9c4f87a65b6c72cce812a2fde83c94ea6fa538
Michael Canonizado <michaelxaviercanonizado@gmail.com>
2024-04-03 01:16:07 +0800

    test: test sign conditions in divideBignum()

--- sandbox.c
+++ sandbox.c
@@ -14,14 +14,17 @@ int main() {
     Bignum res1 = initBignum();
     Bignum res2 = initBignum();
 
-    setBignum(&num1, "123", positive);
-    setBignum(&num2, "123", positive);
-    setBignum(&num3, "123", positive);
-    setBignum(&num4, "987", positive);
+    setBignum(&num1, "123", negative);
+    setBignum(&num2, "123", negative);
+    setBignum(&num3, "123", negative);
+    setBignum(&num4, "987", negative);
+
 
     divideBignum(&res1, &num1, &num2);
     divideBignum(&res2, &num3, &num4);
 
+    printf("\n\nn1: %d | n2:  %d | n3:  %d | n4: %d\n\n", num1.sign, num2.sign, num3.sign, num4.sign);
+    
     printf("\n");
     printBignum(&num1);
     printf(" / ");
@@ -36,6 +39,8 @@ int main() {
     printf(" = ");
     printBignum(&res2);
     printf("\n");
+
+   
 
     clock_t end = clock();
     double cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
