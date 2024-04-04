c0c1c41b8e7f991bdc13a6c2dc036485ba18bb25
deynklarys <144914287+deynklarys@users.noreply.github.com>
2024-04-04 23:18:45 +0800

    feat: read user input for confirmation

--- program/errors.c
+++ program/errors.c
@@ -1,25 +1,33 @@
 #include <ncurses/ncurses.h>
 #include <string.h>
+#include <stdio.h>
 #include <stdlib.h>
 
 // compiling format
-// gcc printf.c -o printf.exe -lncurses -DNCURSES_STATIC
+// cd "c:\Repositories\rsa-cipher-tool\program\" ; if ($?) { gcc program.c -o program -lncurses -DNCURSES_STATIC } ; if ($?) { .\program }
 
 int main() {
     // Initialize ncurses
     initscr();
     refresh();
 
+    // raw();
+    // noecho();
+
     // Get the size of the terminal
     int rows, cols;
     getmaxyx(stdscr, rows, cols);
+    printw("%d\n", rows);
 
     // Calculate the center position
     int x = cols / 2;
-    int y = rows / 2;
+    int y = rows / 10;
+
+    // Print "Hello World" at the center position
+    // mvprintw(y++, x - 6, "Hello World");
 
     // Box outline for the window
-    box(stdscr, 0, 0);
+    // box(stdscr, 0, 0);
 
     // In order to capture special keystrokes like Backspace, Delete and the four arrow keys by getch(), you need to call
     keypad(stdscr, TRUE);
@@ -28,12 +36,12 @@ int main() {
     char user_input[100];
     
     char * optionsArr[5] = {"1) Generate Keys", "2) Encrypt Text", "3) Decrypt Text", "4) About", "5) Exit program"};
-
+    
     char * userChoice[5] = {"GENERATING KEYS", "ENCRYPTING TEXT", "DECRYPTING TEXT", "ABOUT US", "EXIT PROGRAM"};
 
     int i, userInput;
     int lengthArr[5], choiceArr[5];
-        
+
     do {
         for ( i = 0; i < 5; i++)
         {
@@ -45,6 +53,8 @@ int main() {
         echo(); 
         mvprintw(y++, x - 10, "Enter number: ");
         
+
+
         getstr(user_input);
         userInput = atoi(user_input);
 
@@ -52,10 +62,13 @@ int main() {
         for ( i = 0; i < 5; i++)
             choiceArr[i] = strlen(userChoice[i]);
 
+        y = 10;
         switch (userInput)
         {
         case 1:
-            mvprintw(y++, x - choiceArr[0], userChoice[0]);
+            clear();
+            refresh();
+            mvprintw(y, x - choiceArr[0], userChoice[0]);
             refresh();
             mvprintw(y++, x - 20, "You are about to generate an RSA private key");
             refresh();
@@ -72,30 +85,43 @@ int main() {
                 key = getch(); // Get a single character from the user
             } while (key != 'Y' && key != 'y' && key != 'N' && key != 'n');
 
-            if (key == 'Y' || key == 'y')
-                mvprintw(y++, x - 30, "Keys saved successfully!");
-            else
-                mvprintw(y++, x - 30, "Keys not saved!");
+            int enter;
+            do {
+                enter = getch(); // Wait for the user to press ENTER
+            } while (enter != 10);
 
+            if (key == 'Y' || key == 'y')
+            {
+                mvprintw(y++, x - 30, "Keys saved successfully!");
+            }
+            else
+            {
+                mvprintw(y++, x - 30, "Keys not saved!");
+            }
+            clear();
+            refresh(); // Refresh the screen to show the changes
             break;
         case 2:
-            mvprintw(y++, x - choiceArr[1], userChoice[1]);
+            clear();
+            refresh();
+            mvprintw(y, x - choiceArr[1], userChoice[1]);
             break;
         case 3:
-            mvprintw(y++, x - choiceArr[2], userChoice[2]);
+            clear();
+            refresh();
+            mvprintw(y, x - choiceArr[2], userChoice[2]);
             break;
         case 4:
-            mvprintw(y++, x - choiceArr[3], userChoice[3]);
+            clear();
+            refresh();
+            mvprintw(y, x - choiceArr[3], userChoice[3]);
             break;      
         default:
             break;
         }
-        
+
         noecho(); // Disable echoing of input again
     } while (userInput != 5);
-
-    // End ncurses
-    endwin();
 
     return 0;
 }
