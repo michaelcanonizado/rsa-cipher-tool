38f41fa9d84dc115dfcdeaca7fbae59858d4fc55
Michael Canonizado <michaelxaviercanonizado@gmail.com>
2024-04-04 22:27:32 +0800

    feat: add result prints

--- arithmetics/bignum-binary-exponentiation.c
+++ arithmetics/bignum-binary-exponentiation.c
@@ -1,9 +1,36 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #include "../bignum.h"
 
 int main(void) {
+    clock_t start = clock();
 
-    printf("Hello World!");
+    Bignum base = initBignum(); 
+    Bignum exponent = initBignum(); 
+    Bignum binaryExponent = initBignum(); 
+    Bignum result = initBignum();
+
+    setBignum(&base, "123", positive);
+    setBignum(&exponent, "123", positive);
+    
+    printf("\n\n\n");
+    printBignum(&exponent);
+    printf(" in binary: ");
+    printBignum(&binaryExponent);
+    printf("\nBinary Length: %llu", binaryExponent.length);
+
+    printf("\n\n--------------------------------------\n----------------RESULT----------------\n--------------------------------------\n\n");
+    printBignum(&base);
+    printf("  ^  ");
+    printBignum(&exponent);
+    printf("  =  ");
+    printBignum(&result);
+
+    
+    clock_t end = clock();
+    double cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+    printf("\n\n\nCPU time used: %f seconds\n\n", cpu_time_used);
 
     return 0;
 }
