3157b8b77647c4063a9f8189f17e676c7cf55f3a
Michael Canonizado <michaelxaviercanonizado@gmail.com>
2024-04-07 21:40:42 +0800

    test: test changes in addBignum()

--- sandbox.c
+++ sandbox.c
@@ -15,14 +15,14 @@ int main() {
     Bignum res1 = initBignum();
     Bignum res2 = initBignum();
 
-    setBignum(&num1, "567", positive);
-    setBignum(&num2, "121", positive);
+    setBignum(&num1, "0", positive);
+    setBignum(&num2, "0", positive);
 
-    halfBignum(&res1, &num1);
+    addBignum(&res1, &num1, &num2);
     
     printf("\n");
     printBignum(&num1);
-    printf(" %% ");
+    printf(" + ");
     printBignum(&num2);
     printf(" = ");
     printBignum(&res1);
@@ -30,13 +30,11 @@ int main() {
 
     printf("\n");
     printBignum(&num3);
-    printf(" %% ");
+    printf(" + ");
     printBignum(&num4);
     printf(" = ");
     printBignum(&res2);
     printf("\n");
-
-    printf("\n%d %% %d = %d", 1212, 121, 1212 % 121);
 
     clock_t end = clock();
     double cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
