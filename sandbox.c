cf1c5dba67a878fb7c639ba6a609afa62fc24e33
Michael Canonizado <michaelxaviercanonizado@gmail.com>
2024-04-08 02:05:57 +0800

    test: test the migration changes from static array to dynamic array in Bignum.digits[] in bignum.c

--- sandbox.c
+++ sandbox.c
@@ -8,37 +8,27 @@ int main() {
     clock_t start = clock();
 
     Bignum num1 = initBignum();
-    Bignum num2 = initBignum(); 
-    Bignum num3 = initBignum();
-    Bignum num4 = initBignum(); 
-
+    Bignum num2 = initBignum();
     Bignum res1 = initBignum();
-    Bignum res2 = initBignum();
 
-    setBignum(&num1, "000000000", positive);
-    setBignum(&num2, "99999-9999", negative);
+    setBignum(&num1, "12345", positive);
+    setBignum(&num2, "98765", positive);
 
-    subtractBignum(&res1, &num1, &num2);
-    
-    printf("\n");
+    addBignum(&res1, &num1, &num2);
+
+    printf("\n\n");
     printBignum(&num1);
-    printf(" - ");
+    printf(" + ");
     printBignum(&num2);
     printf(" = ");
     printBignum(&res1);
     printf("\n");
 
-    printf("\n");
-    printBignum(&num3);
-    printf(" - ");
-    printBignum(&num4);
-    printf(" = ");
-    printBignum(&res2);
-    printf("\n");
+    freeBignums();
 
     clock_t end = clock();
     double cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
-    printf("\nCPU time used: %f seconds\n\n", cpu_time_used);
+    printf("\n\nCPU time used: %f seconds\n\n", cpu_time_used);
 
     return 0;
 }
