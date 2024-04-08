b33228c968bcee1e016c1fd826f649926eab7054
deynklarys <144914287+deynklarys@users.noreply.github.com>
2024-04-08 21:39:38 +0800

    feat: add clearScreen function

--- program/program.c
+++ program/program.c
@@ -1,8 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+void clearScreen() {
+    #ifdef _WIN32
+        system("cls");
+    #else
+        system("clear");
+    #endif
+}
+
+// This function waits for the user to press ENTER to avoid the program from closing immediately after the user's input
+void waitForEnter() {
+    int enter;
+    do {
+        enter = getchar(); // Wait for the user to press ENTER
+    } while (enter != 10);
+}
 
 int main (){
+    
+    clearScreen();
 
-
+    
 
 
 
