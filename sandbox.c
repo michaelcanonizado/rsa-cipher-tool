b1f8854adcd25222c65729478211f5f6c1056d88
Michael Canonizado <michaelxaviercanonizado@gmail.com>
2024-04-08 21:59:07 +0800

    test: test dynamic memoery allocation migration changes in bignum.c

--- sandbox.c
+++ sandbox.c
@@ -7,24 +7,43 @@
 int main() {
     clock_t start = clock();
 
-    Bignum num1 = initBignum();
-    Bignum num2 = initBignum();
-    Bignum res1 = initBignum();
+    Bignum num1;
+    Bignum num2;
+    Bignum res1;
 
-    setBignum(&num1, "12123123", negative);
-    setBignum(&num2, "9873", negative);
+    printf("\n......................INITIALIZEING BIGNUMS.................\n");
 
-    subtractBignum(&res1, &num1, &num2);
+    initBignum(&num1);
+    initBignum(&num2);
+    initBignum(&res1);
+
+    printf("\n\n\n......................ADDRESS OF BIGNUMS....................\n");
+
+    printf("\nnum1 %p", &num1);
+    printf("\nnum2 %p", &num2);
+    printf("\nres1 %p", &res1);
+    
+
+    printf("\n\n\n......................SETTING OF BIGNUMS.....................\n");
+
+    setBignum(&num1, "9876234267", positive);
+    setBignum(&num2, "12", positive);
+
+    printf("\n\n\n....................PERFORMING OPERATIONS....................\n");
+
+    multiplyBignum(&res1, &num1, &num2);
 
     printf("\n\n");
     printBignum(&num1);
-    printf(" + ");
+    printf(" ~ ");
     printBignum(&num2);
     printf(" = ");
     printBignum(&res1);
     printf("\n");
 
-    freeBignums();
+    printf("\n\n\n......................FREEING BIGNUMS.........................\n");
+
+    freeAllBignums();
 
     clock_t end = clock();
     double cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
