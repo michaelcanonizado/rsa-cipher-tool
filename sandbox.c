8f9f5b5e897e3667f96366b27babc66d5aa9ffeb
Michael Canonizado <michaelxaviercanonizado@gmail.com>
2024-04-10 18:18:02 +0800

    tested: dynamic memory allocation migration in initBignum(), freeBignum(), and freeAllBignums()

--- sandbox.c
+++ sandbox.c
@@ -10,29 +10,32 @@ int main() {
     Bignum num1;
     Bignum num2;
     Bignum res1;
+    Bignum res2;
 
     printf("\n......................INITIALIZEING BIGNUMS.................\n");
 
     initBignum(&num1);
     initBignum(&num2);
     initBignum(&res1);
+    initBignum(&res2);
 
     printf("\n\n\n......................ADDRESS OF BIGNUMS....................\n");
 
-    printf("\nnum1 %p", &num1);
-    printf("\nnum2 %p", &num2);
-    printf("\nres1 %p", &res1);
+    printf("\nnum1 %p.%p", &num1, num1.digits);
+    printf("\nnum2 %p.%p", &num2, num2.digits);
+    printf("\nres1 %p.%p", &res1, res1.digits);
+    printf("\nres1 %p.%p", &res2, res2.digits);
     
 
     printf("\n\n\n......................SETTING OF BIGNUMS.....................\n");
 
-    setBignum(&num1, "92342121231231123112312323", positive);
-    setBignum(&num2, "223", positive);
+    setBignum(&num1, "98761231231231233123123124123123123123", positive);
+    setBignum(&num2, "2", positive);
 
     printf("\n\n\n....................PERFORMING OPERATIONS....................\n");
 
-    // divideBignum(&res1, &num1, &num2);
-    multiplyBignum(&res1, &num1, &num2);
+    divideBignum(&res1, &num1, &num2);
+    halfBignum(&res2, &num1);
 
     printf("\n\n");
     printBignum(&num1);
@@ -40,12 +43,21 @@ int main() {
     printBignum(&num2);
     printf(" = ");
     printBignum(&res1);
+
+    printf("\n\n");
+    printBignum(&num1);
+    printf(" ~ ");
+    printBignum(&num2);
+    printf(" = ");
+    printBignum(&res2);
     printf("\n");
 
     printf("\n\n\n......................FREEING BIGNUMS.........................\n");
 
+    printBignumNodeList();
 
     freeAllBignums();
+
 
     clock_t end = clock();
     double cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
