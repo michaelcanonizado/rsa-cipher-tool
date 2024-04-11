22b3e1bdb7ca8771bcc129b653dda737a0ce5a2f
Michael Canonizado <michaelxaviercanonizado@gmail.com>
2024-04-11 19:56:37 +0800

    feat: add bignumBinaryModularExponentiationCompressed() which has no comments and prints

--- arithmetics/bignum-binary-modular-exponentiation.c
+++ arithmetics/bignum-binary-modular-exponentiation.c
@@ -118,6 +118,68 @@ int bignumBinaryModularExponentiation(Bignum *result, Bignum *base, Bignum *bina
 
     freeBignum(&remainderSquaredModDivisorTimesBase);
     freeBignum(&remainderSquaredModDivisorTimesBaseModDivisor);
+
+    return 0;
+}
+
+int bignumBinaryModularExponentiationCompressed(Bignum *result, Bignum *base, Bignum *binaryExponent, Bignum *divisor) {
+
+    Bignum remainder;
+    initBignum(&remainder);
+    copyBignum(&remainder, base);
+
+    Bignum remainderSquared;
+    Bignum remainderSquaredModDivisor;
+    initBignum(&remainderSquared);
+    initBignum(&remainderSquaredModDivisor);
+
+    Bignum tempRemainder;
+    initBignum(&tempRemainder);
+
+    Bignum remainderSquaredModDivisorTimesBase;
+    Bignum remainderSquaredModDivisorTimesBaseModDivisor;
+    initBignum(&remainderSquaredModDivisorTimesBase);
+    initBignum(&remainderSquaredModDivisorTimesBaseModDivisor);
+
+    for (int i = binaryExponent->length - 1; i >= 0; i--) {
+        resetBignum(&remainderSquared);
+        resetBignum(&remainderSquaredModDivisor);
+
+        if (binaryExponent->digits[i - 1] == 0) {
+            resetBignum(&tempRemainder);
+            copyBignum(&tempRemainder, &remainder);
+
+            multiplyBignum(&remainderSquared, &remainder, &remainder);
+            moduloBignum(&remainderSquaredModDivisor, &remainderSquared, divisor);
+
+            copyBignum(&remainder, &remainderSquaredModDivisor);
+        } else if (binaryExponent->digits[i - 1] == 1) {
+            resetBignum(&remainderSquaredModDivisorTimesBase);
+            resetBignum(&remainderSquaredModDivisorTimesBaseModDivisor);
+
+            multiplyBignum(&remainderSquared, &remainder, &remainder);
+            moduloBignum(&remainderSquaredModDivisor, &remainderSquared, divisor);
+            
+            multiplyBignum(&remainderSquaredModDivisorTimesBase, &remainderSquaredModDivisor, base);
+            moduloBignum(&remainderSquaredModDivisorTimesBaseModDivisor, &remainderSquaredModDivisorTimesBase,divisor);
+
+            copyBignum(&remainder, &remainderSquaredModDivisorTimesBaseModDivisor);
+        }
+    }
+
+    copyBignum(result, &remainder);
+
+    freeBignum(&remainder);
+
+    freeBignum(&remainderSquared);
+    freeBignum(&remainderSquaredModDivisor);
+
+    freeBignum(&tempRemainder);
+
+    freeBignum(&remainderSquaredModDivisorTimesBase);
+    freeBignum(&remainderSquaredModDivisorTimesBaseModDivisor);
+
+    return 0;
 }
 
 int main() {
@@ -135,12 +197,12 @@ int main() {
     initBignum(&divisor); 
     initBignum(&result);
 
-    setBignum(&base, "3123972", positive);
-    setBignum(&exponent, "23", positive);
-    setBignum(&divisor, "4121", positive);
+    setBignum(&base, "72", positive);
+    setBignum(&exponent, "231291283937123014839371230149089123912390891239123", positive);
+    setBignum(&divisor, "4121136789122390891239135124123", positive);
 
     bignumToBinary(&binaryExponent, &exponent);
-    bignumBinaryModularExponentiation(&result, &base, &binaryExponent, &divisor);
+    bignumBinaryModularExponentiationCompressed(&result, &base, &binaryExponent, &divisor);
     
     printf("\n\n\n");
     printBignum(&exponent);
