fa7497b7ed32e4dd2e4128da27074811425b21e7
Michael Canonizado <michaelxaviercanonizado@gmail.com>
2024-04-13 22:24:39 +0800

    test: test random numbers generated by generatePrimeBignum()

--- sandbox.c
+++ sandbox.c
@@ -7,46 +7,26 @@
 int main() {
     clock_t start = clock();
 
-    Bignum base;
-    Bignum exponent;
-    Bignum divisor;
-    Bignum result;
+    Bignum num;
 
     printf("\n......................INITIALIZEING BIGNUMS.................\n");
 
-    initBignum(&base);
-    initBignum(&exponent);
-    initBignum(&divisor);
-    initBignum(&result);
+    initBignum(&num);
 
     printf("\n\n\n......................ADDRESS OF BIGNUMS....................\n");
 
-    printf("\nbase %p.%p", &base, base.digits);
-    printf("\nexponent %p.%p", &exponent, exponent.digits);
-    printf("\ndivisor %p.%p", &divisor, divisor.digits);
-    printf("\nresult %p.%p", &result, result.digits);
+    printf("\nnum %p.%p", &num, num.digits);
     
     printf("\n\n\n......................SETTING OF BIGNUMS.....................\n");
 
-    // setBignum(&base, "83", positive);
-    // setBignum(&exponent, "2312", positive);
-    // setBignum(&divisor, "2313", positive);
-    setBignum(&base, "8393718", positive);
-    setBignum(&exponent, "231393718393712309371213937138", positive);
-    setBignum(&divisor, "231393718393712309371213937139", positive);
-
     printf("\n\n\n....................PERFORMING OPERATIONS....................\n");
 
-    modularExponentiationBignum(&result, &base, &exponent, &divisor);
+    unsigned long long int primeLength = 20;
 
-    printf("\n");
-    printBignum(&base);
-    printf("\n^\n");
-    printBignum(&exponent);
-    printf("\nmod\n");
-    printBignum(&divisor);
-    printf("\n=\n");
-    printBignum(&result);
+    generatePrimeBignum(&num, primeLength);
+
+    printf("\nnum: ");
+    printBignumExtended(&num, primeLength + 5);
 
     printf("\n\n\n......................FREEING BIGNUMS.........................\n");
 
