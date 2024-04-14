3cfd2c33d48afa010cf736f806f6176b27f1fcd4
Michael Canonizado <michaelxaviercanonizado@gmail.com>
2024-04-15 00:49:11 +0800

    chore: add prints on each iteration

--- miller-rabin.c
+++ miller-rabin.c
@@ -51,35 +51,56 @@ int Miller(long long p,int iteration)
     {
         return 0;
     }
-     s = p - 1;
+    s = p - 1;
     while (s % 2 == 0)
     {
+        printf("\ns/2: %lld", s);
         s /= 2;
     }
-    for (i = 0; i < iteration; i++)
-    {
-        long long a = rand() % (p - 1) + 1, temp = s;
+    printf("\nfinal s/2: %lld", s);
+
+    // for (i = 0; i < iteration; i++)
+    // {
+    //     long long a = rand() % (p - 1) + 1, temp = s;
+    //     long long mod = modulo(a, temp, p);
+    //     while (temp != p - 1 && mod != 1 && mod != p - 1)
+    //     {
+    //         mod = mulmod(mod, mod, p);
+    //         temp *= 2;
+    //     }
+    //     if (mod != p - 1 && temp % 2 == 0)
+    //     {
+    //         return 0;
+    //     }
+    // }
+        long long a = 2, temp = s;
         long long mod = modulo(a, temp, p);
         while (temp != p - 1 && mod != 1 && mod != p - 1)
         {
+            printf("\n( %lld * %lld ) mod %lld = %lld", mod, mod, p, mulmod(mod, mod, p));
             mod = mulmod(mod, mod, p);
+
+            printf("\n%lld * 2 = %lld", temp, temp*2);
             temp *= 2;
         }
         if (mod != p - 1 && temp % 2 == 0)
         {
             return 0;
         }
-    }
+
     return 1;
 }
 //Main
 int main()
 {
     int iteration = 5;
-    long long num = 14081;
+    long long num = 561;
     if (Miller( num, iteration))
         printf("\n%d - %lld is prime\n", Miller( num, iteration), num);
     else
         printf("\n%d - %lld is not prime\n", Miller( num, iteration), num);
+
+    printf("\n\nMulMod: %lld", mulmod(123,9995,49));
+
     return 0;
 }
