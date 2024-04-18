7524217544a5d62d17a0596767459de042a55ee3
deynklarys <144914287+deynklarys@users.noreply.github.com>
2024-04-18 19:23:15 +0800

    feat: loadingBar() from https://www.youtube.com/watch?v=t_vM_8TLjFE

--- src/program/loading-bar.c
+++ src/program/loading-bar.c
@@ -1,47 +1,49 @@
 // C program to create loading bar 
 #include <stdio.h> 
-#include <windows.h> 
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_STRING_LENGTH 1000000
+
+const int PROGRESS_BAR_LENGTH = 50;
 
 // Function to creating loading bar 
-void loadingBar() 
-{ 
-	// 0 - black background, 
-	// A - Green Foreground 
-	system("color 0A"); 
+void loadingBar(int percentDone) { 
+	int numChar = percentDone * PROGRESS_BAR_LENGTH / 100;
+	int numSpace = PROGRESS_BAR_LENGTH - numChar;
 
-	// Initialize char for printing 
-	// loading bar 
-	char a = 177, b = 219; 
+	printf("\r[");
+	for (int i = 0; i < numChar; i++) {
+		printf("#");
+	}
+	for (int i = 0; i < numSpace; i++) {
+		printf(" ");
+	}
+	printf("] %d%% Done", percentDone);
+	fflush(stdout);
+}
 
-	printf("\n\n\n\n\t\t\t\t\tLoading...\n\n"); 
-	printf("\t\t\t\t\t"); 
-
-	// Print initial loading bar 
-	for (int i = 0; i < 26; i++) 
-		printf("%c", a); 
-
-	// Set the cursor again starting 
-	// point of loading bar 
-	printf("\r"); 
-	printf("\t\t\t\t\t"); 
-
-	// Print loading bar progress 
-	for (int i = 0; i < 26; i++) { 
-		printf("%c", b); 
-
-		// Sleep for 1 second 
-		Sleep(1000); 
-	} 
-} 
+// Function to copy a very long string one character at a time
+void convertToASCII(const char* str) {
+	int length = strlen(str);
+	for (int i = 0; i < length; i++) {
+		// printf("%d", str[i]);
+		if (i != length - 1) {
+			// printf(", ");
+		}
+	}
+}
 
 // Driver Code 
 int main() 
 { 
-    // Function Call 
-    loadingBar(); 
-
-    // Reset the color of the terminal text to the default
-    system("color 07");
-
-    return 0; 
+	char plainText[MAX_STRING_LENGTH] = "You are about to generate an RSA private key with this option. Additionally, it will display the public key of a generated private key. Do you agree to save a copy of your private and public keys? [Y/N]. Encryption includes the message to be encrypted and the public key of the recipient. The txt file of the message must be in the same folder of the C program. Is the txt file in the same folder of the C program? [Y/N] Do you have the public key of the recipient? [Y/N] The RSA Cipher Tool program generates, encrypts, and decrypts messages using the RSA algorithm. The program uses the RSA (Rivest, Shamir, Adleman) Algorithm to generate public and private keys needed to encrypt and decrypt messages and a custom bignum library to handle large numbers. The user will use the program to encrypt a message in a file, where it will then return the encrypted/secret message. Then, they can now send it to the recipient of the message safely, where they can also use the same program to decrypt the message and reveal the actual message. This is a programming project for Computer Programming 2 during the Academic Year 2023-2024 at the Bicol University College of Science.";
+	int plainTextLength = strlen(plainText);
+	printf("Length: %d\n", plainTextLength);
+	loadingBar(0);
+	for (int i = 0; i < plainTextLength; i++) {
+		convertToASCII(plainText);
+		loadingBar((i+1)*100/plainTextLength);
+	}
+	return 0; 
 }
