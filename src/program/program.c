57923b44ea421d5364a4571343077b98b9eee5f8
unknown <michaelxaviercanonizado@gmail.com>
2024-04-20 03:28:01 +0800

    fix(global): make width a global variable instead of passing it as paramters in functions

--- src/program/program.c
+++ src/program/program.c
@@ -4,6 +4,9 @@
 #include <time.h>
 #include <ctype.h>
 
+int currTerminalWidth = 0;
+int currTerminalHeight = 0;
+
 // Includes the appropriate header file to use operating system-specific functions. This is useful for functions like clearing the screen, moving the cursor, and getting the terminal size.
 #ifdef _WIN32
 	#include <windows.h>
@@ -49,22 +52,21 @@ FILE *file;
 int main() {
 	clearScreen();
 
-	int width, height;
-	getTerminalSize(&width, &height);
-	printf("Width: %d\nHeight: %d\n", width, height);
+	getTerminalSize(&currTerminalWidth, &currTerminalHeight);
+	printf("Width: %d\nHeight: %d\n", currTerminalWidth, currTerminalHeight);
 
 	// Since the horizontal positioning of the outputs can be adjusted by incrementing the y-axis, this integer variable stores value of the height divided by 3 as all the outputs starts to be displayed at this value. This can't be applied to the x-axis as the outputs are displayed at the center of the screen. The x-axis is adjusted by subtracting the length of the string from the width of the screen and dividing the result by 2. It is depedent on the length of the string to be displayed.
-	int adjustedHeight = height / 3;
+	int adjustedHeight = currTerminalHeight / 3;
 	int i, userInput;
 
 	do {
 		char *optionsArr[5] = {"1) Generate Keys", "2) Encrypt Text", "3) Decrypt Text", "4) About", "5) Exit program"};
 		for ( i = 0; i < 5; i++) {
-			moveCursor((width - 14) / 2, adjustedHeight + i);
+			moveCursor((currTerminalWidth - 14) / 2, adjustedHeight + i);
 			printf("%s\n", optionsArr[i]);
 		}
 
-		moveCursor((width - 10)/ 2, adjustedHeight + i);
+		moveCursor((currTerminalWidth - 10)/ 2, adjustedHeight + i);
 		printf("Enter number: ");
 		scanf("%d", &userInput);
 		// Consume any extra characters in the input buffer to prevent an infinite loop
@@ -72,28 +74,28 @@ int main() {
 
 		switch (userInput) {
 			case 1:
-				generateKeys(width, adjustedHeight, i);
+				generateKeys(currTerminalWidth, adjustedHeight, i);
 				break;
 			case 2:
-				encryptText(width, adjustedHeight);
+				encryptText(currTerminalWidth, adjustedHeight);
 				break;
 			case 3:
-				decryptText(width, adjustedHeight);
+				decryptText(currTerminalWidth, adjustedHeight);
 				break;
 			case 4:
-				aboutProgram(width, height);
+				aboutProgram(currTerminalWidth, currTerminalHeight);
 				break;
 			case 5:
 				clearScreen();
-				moveCursor((width - 20) / 2, adjustedHeight + 5);
+				moveCursor((currTerminalWidth - 20) / 2, adjustedHeight + 5);
 				printf("Exiting program...\n");
 				sleepProgram(500);
 				break;
 			default:
 				clearScreen();
-				moveCursor((width - 15) / 2, adjustedHeight + 1);
+				moveCursor((currTerminalWidth - 15) / 2, adjustedHeight + 1);
 				printf("Invalid input.");
-				moveCursor((width - 39) / 2, adjustedHeight + 2);
+				moveCursor((currTerminalWidth - 39) / 2, adjustedHeight + 2);
 				printf("Please enter a number between 1 and 5.");
 				sleepProgram(1000);
 				clearScreen();
@@ -108,8 +110,8 @@ void getTerminalSize(int *width, int *height) {
 #ifdef _WIN32
 	CONSOLE_SCREEN_BUFFER_INFO csbi;
 	GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
-	*width = csbi.srWindow.Right - csbi.srWindow.Left + 1;
-	*height = csbi.srWindow.Bottom - csbi.srWindow.Top + 1;
+	currTerminalWidth = csbi.srWindow.Right - csbi.srWindow.Left + 1;
+	currTerminalHeight = csbi.srWindow.Bottom - csbi.srWindow.Top + 1;
 #else
 	struct winsize size;
 	ioctl(STDOUT_FILENO, TIOCGWINSZ, &size);
@@ -157,8 +159,8 @@ void clearLines(int startLine, int endLine, int width) {
 void waitForDone(int width, int height) {
 	char done[100];
 	do {
-		clearLines(height - 2, height - 2, width);
-		moveCursor((width - 21)/ 2, height - 2);
+		clearLines(currTerminalHeight - 2, currTerminalHeight - 2, currTerminalWidth);
+		moveCursor((currTerminalWidth - 21)/ 2, currTerminalHeight - 2);
 		printf("Enter DONE to back: ");
 		// Get the user's input from the standard input stream	
 		fgets(done, sizeof(done), stdin); 
@@ -178,7 +180,7 @@ void waitForDone(int width, int height) {
 			done[i] = tolower(done[i]);
 		}
 
-		moveCursor((width - 21)/ 2, height - 2);
+		moveCursor((currTerminalWidth - 21)/ 2, currTerminalHeight - 2);
 
 	} while (strcmp(done, "done") != 0);
 }
@@ -190,9 +192,9 @@ char getConfirm(int width, int adjustedHeight, int offsetY) {
 		confirm = getchar();
 	}
 	if (confirm != 'Y' && confirm != 'y' && confirm != 'N' && confirm != 'n') {
-		moveCursor((width - 37) / 2, adjustedHeight + offsetY);
+		moveCursor((currTerminalWidth - 37) / 2, adjustedHeight + offsetY);
 		printf("Invalid input. Please enter Y or N.");
-		moveCursor((width - 37) / 2, adjustedHeight + offsetY);
+		moveCursor((currTerminalWidth - 37) / 2, adjustedHeight + offsetY);
 	}
 
 	// Consume any extra characters in the input buffer
@@ -204,7 +206,7 @@ char getConfirm(int width, int adjustedHeight, int offsetY) {
 int displayMessage(char *message[], int count, int width, int adjustedHeight) {
 	int i;
 	for ( i = 0; i < count; i++) {
-		moveCursor((width - strlen(message[i])) / 2, adjustedHeight + i);
+		moveCursor((currTerminalWidth - strlen(message[i])) / 2, adjustedHeight + i);
 		printf("%s\n", message[i]);
 	}
 
@@ -213,7 +215,7 @@ int displayMessage(char *message[], int count, int width, int adjustedHeight) {
 
 void displayAbout(char *message[], int count, int width, int height, int offsetY) {
 	for (int i = 0; i < count; i++) {
-		moveCursor((width - strlen(message[i])) / 2, height / 4 + i + offsetY);
+		moveCursor((currTerminalWidth - strlen(message[i])) / 2, currTerminalHeight / 4 + i + offsetY);
 		printf("%s\n", message[i]);
 	}
 }
@@ -222,15 +224,15 @@ void generateKeys(int width, int adjustedHeight, int i) {
 	clearScreen();
 	char *msgGenerate[] = {"You are about to generate an RSA key", "this option. This will display a private key", "and public key of the generated private key."};
 	int countGenerate = sizeof(msgGenerate) / sizeof(msgGenerate[0]);
-	offsetY = displayMessage(msgGenerate, countGenerate, width, adjustedHeight);
+	offsetY = displayMessage(msgGenerate, countGenerate, currTerminalWidth, adjustedHeight);
 
 	do{
-		clearLines(adjustedHeight + offsetY, adjustedHeight + offsetY + 1, width);
-		moveCursor((width - 37)/ 2, adjustedHeight + offsetY);
+		clearLines(adjustedHeight + offsetY, adjustedHeight + offsetY + 1, currTerminalWidth);
+		moveCursor((currTerminalWidth - 37)/ 2, adjustedHeight + offsetY);
 		printf("Do you agree to save a copy of your\n");
-		moveCursor((width - 32)/ 2, adjustedHeight + offsetY + 1);
+		moveCursor((currTerminalWidth - 32)/ 2, adjustedHeight + offsetY + 1);
 		printf("private and public keys? [Y/N] ");
-		confirm = getConfirm(width, adjustedHeight, offsetY + 1);
+		confirm = getConfirm(currTerminalWidth, adjustedHeight, offsetY + 1);
 
 	} while (confirm != 'Y' && confirm != 'y' && confirm != 'N' && confirm != 'n');
 
@@ -239,14 +241,14 @@ void generateKeys(int width, int adjustedHeight, int i) {
 		
 		// At this point, function calls can be made to generate the keys. For now, the program will display a message that the keys are generated.
 
-		moveCursor((width - 30)/ 2, adjustedHeight);
+		moveCursor((currTerminalWidth - 30)/ 2, adjustedHeight);
 		printf("Keys generated successfully!\n");
 	} else {
-		moveCursor((width - 25)/ 2, adjustedHeight);
+		moveCursor((currTerminalWidth - 25)/ 2, adjustedHeight);
 		printf("Keys generation failed!\n");
 	}
 
-	waitForDone(width, adjustedHeight * 3);
+	waitForDone(currTerminalWidth, adjustedHeight * 3);
 	clearScreen();
 }
 
@@ -256,13 +258,13 @@ void encryptText(int width, int adjustedHeight) {
 	clearScreen();
 	char* msgEncrypt[] = {"Encryption includes the message to be encrypted and", "the public key of the recipient. The txt file of the", "message must be in the same folder of the C program."};
 	int countEncrypt = sizeof(msgEncrypt) / sizeof(msgEncrypt[0]);
-	offsetY = displayMessage(msgEncrypt, countEncrypt, width, adjustedHeight);
+	offsetY = displayMessage(msgEncrypt, countEncrypt, currTerminalWidth, adjustedHeight);
 
 	do {
-		clearLines(adjustedHeight + offsetY, adjustedHeight + offsetY, width);
-		moveCursor((width - 60)/ 2, adjustedHeight + offsetY);
+		clearLines(adjustedHeight + offsetY, adjustedHeight + offsetY, currTerminalWidth);
+		moveCursor((currTerminalWidth - 60)/ 2, adjustedHeight + offsetY);
 		printf("Is the txt file in the same folder of the C program? [Y/N] ");
-		confirm = getConfirm(width, adjustedHeight, offsetY + 1);
+		confirm = getConfirm(currTerminalWidth, adjustedHeight, offsetY + 1);
 
 	} while (confirm != 'Y' && confirm != 'y' && confirm != 'N' && confirm != 'n');
 
@@ -279,8 +281,8 @@ void encryptText(int width, int adjustedHeight) {
 			clearScreen();
 
 			do {
-				clearLines(adjustedHeight, adjustedHeight, width);
-				moveCursor((width - 38)/ 2, adjustedHeight);
+				clearLines(adjustedHeight, adjustedHeight, currTerminalWidth);
+				moveCursor((currTerminalWidth - 38)/ 2, adjustedHeight);
 				printf("Enter file name: ");
 				fgets(fileName, sizeof(fileName), stdin);
 
@@ -294,7 +296,7 @@ void encryptText(int width, int adjustedHeight) {
 		
 				if (file == NULL) {
 					// If the file could not be opened, print an error message
-					moveCursor((width - 38)/ 2, adjustedHeight + offsetY + 3);
+					moveCursor((currTerminalWidth - 38)/ 2, adjustedHeight + offsetY + 3);
 					printf("Could not open file. Please try again.\n");
 				}
 			} while (file == NULL);
@@ -307,7 +309,7 @@ void encryptText(int width, int adjustedHeight) {
 					fprintf(stderr, "Failed to allocate memory for publicKEY\n");
 					exit(1);
 			}
-			moveCursor((width - 38)/ 2, adjustedHeight + 1);
+			moveCursor((currTerminalWidth - 38)/ 2, adjustedHeight + 1);
 			printf("Enter public key: ");
 			scanf("%s", publicKEY);
 
@@ -324,27 +326,27 @@ void encryptText(int width, int adjustedHeight) {
 			clock_t end = clock();
 			double cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
 
-			moveCursor((width - 30)/ 2, adjustedHeight);
+			moveCursor((currTerminalWidth - 30)/ 2, adjustedHeight);
 			printf("Message encryption success!\n");
-			moveCursor((width - 30)/ 2, adjustedHeight + 1);
+			moveCursor((currTerminalWidth - 30)/ 2, adjustedHeight + 1);
 			printf("Function executed in: %f seconds\n", cpu_time_used);
-			moveCursor((width - 30)/ 2, adjustedHeight + 2);
+			moveCursor((currTerminalWidth - 30)/ 2, adjustedHeight + 2);
 			printf("Encrypted ____ characters in __ seconds\n");
 
 			fclose(file);
 		} else {
 			clearScreen();
-			moveCursor((width - 25)/ 2, adjustedHeight);
+			moveCursor((currTerminalWidth - 25)/ 2, adjustedHeight);
 			printf("Message encryption failed!\n");
 		}
 	}
 	else {
 			clearScreen();
-			moveCursor((width - 25)/ 2, adjustedHeight);
+			moveCursor((currTerminalWidth - 25)/ 2, adjustedHeight);
 			printf("Message encryption failed!\n");
 	}
 
-	waitForDone(width, adjustedHeight * 3);
+	waitForDone(currTerminalWidth, adjustedHeight * 3);
 	clearScreen();
 
 	free(publicKEY);
@@ -356,22 +358,22 @@ void decryptText(int width, int adjustedHeight) {
 	clearScreen();
 	char* msgDecrypt[] = {"Decryption includes the message to be decrypted and", "the private key of the recipient. The txt file of the", "message must be in the same folder of the C program."};
 	int countDecrypt = sizeof(msgDecrypt) / sizeof(msgDecrypt[0]);
-	offsetY = displayMessage(msgDecrypt, countDecrypt, width, adjustedHeight);
+	offsetY = displayMessage(msgDecrypt, countDecrypt, currTerminalWidth, adjustedHeight);
 
 	do {
-		clearLines(adjustedHeight + offsetY, adjustedHeight + offsetY, width);
-		moveCursor((width - 60)/ 2, adjustedHeight + offsetY);
+		clearLines(adjustedHeight + offsetY, adjustedHeight + offsetY, currTerminalWidth);
+		moveCursor((currTerminalWidth - 60)/ 2, adjustedHeight + offsetY);
 		printf("Is the txt file in the same folder of the C program? [Y/N] ");
-		confirm = getConfirm(width, adjustedHeight, offsetY);
+		confirm = getConfirm(currTerminalWidth, adjustedHeight, offsetY);
 
 	} while (confirm != 'Y' && confirm != 'y' && confirm != 'N' && confirm != 'n');
 
 	if (confirm == 'Y' || confirm == 'y') {
 		do {
-			clearLines(adjustedHeight + offsetY + 1, adjustedHeight + offsetY + 2, width);
-			moveCursor((width - 38)/ 2, adjustedHeight + offsetY + 1);
+			clearLines(adjustedHeight + offsetY + 1, adjustedHeight + offsetY + 2, currTerminalWidth);
+			moveCursor((currTerminalWidth - 38)/ 2, adjustedHeight + offsetY + 1);
 			printf("Do you have your private key? [Y/N] ");
-			confirm = getConfirm(width, adjustedHeight, offsetY + 1);
+			confirm = getConfirm(currTerminalWidth, adjustedHeight, offsetY + 1);
 
 		} while (confirm != 'Y' && confirm != 'y' && confirm != 'N' && confirm != 'n');
 
@@ -379,8 +381,8 @@ void decryptText(int width, int adjustedHeight) {
 			clearScreen();
 
 			do {
-				clearLines(adjustedHeight, adjustedHeight, width);
-				moveCursor((width - 38)/ 2, adjustedHeight);
+				clearLines(adjustedHeight, adjustedHeight, currTerminalWidth);
+				moveCursor((currTerminalWidth - 38)/ 2, adjustedHeight);
 				printf("Enter file name: ");
 				fgets(fileName, sizeof(fileName), stdin);
 
@@ -394,7 +396,7 @@ void decryptText(int width, int adjustedHeight) {
 		
 				if (file == NULL) {
 					// If the file could not be opened, print an error message
-					moveCursor((width - 38)/ 2, adjustedHeight + offsetY + 3);
+					moveCursor((currTerminalWidth - 38)/ 2, adjustedHeight + offsetY + 3);
 					printf("Could not open file. Please try again.\n");
 				}
 			} while (file == NULL);
@@ -407,7 +409,7 @@ void decryptText(int width, int adjustedHeight) {
 					fprintf(stderr, "Failed to allocate memory for privateKEY\n");
 					exit(1);
 			}
-			moveCursor((width - 38)/ 2, adjustedHeight + 1);
+			moveCursor((currTerminalWidth - 38)/ 2, adjustedHeight + 1);
 			printf("Enter private key: ");
 			scanf("%s", privateKEY);
 
@@ -424,27 +426,27 @@ void decryptText(int width, int adjustedHeight) {
 			clock_t end = clock();
 			double cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
 
-			moveCursor((width - 30)/ 2, adjustedHeight);
+			moveCursor((currTerminalWidth - 30)/ 2, adjustedHeight);
 			printf("Message encryption success!\n");
-			moveCursor((width - 30)/ 2, adjustedHeight + 1);
+			moveCursor((currTerminalWidth - 30)/ 2, adjustedHeight + 1);
 			printf("Function executed in: %f seconds\n", cpu_time_used);
-			moveCursor((width - 30)/ 2, adjustedHeight + 2);
+			moveCursor((currTerminalWidth - 30)/ 2, adjustedHeight + 2);
 			printf("Decrypted ____ characters in __ seconds\n");
 
 			fclose(file);
 		} else {
 			clearScreen();
-			moveCursor((width - 25)/ 2, adjustedHeight);
+			moveCursor((currTerminalWidth - 25)/ 2, adjustedHeight);
 			printf("Message encryption failed!\n");
 		}
 	}
 	else {
 			clearScreen();
-			moveCursor((width - 25)/ 2, adjustedHeight);
+			moveCursor((currTerminalWidth - 25)/ 2, adjustedHeight);
 			printf("Message encryption failed!\n");
 	}
 
-	waitForDone(width, adjustedHeight * 3);
+	waitForDone(currTerminalWidth, adjustedHeight * 3);
 	clearScreen();
 
 	free(privateKEY);
@@ -452,25 +454,25 @@ void decryptText(int width, int adjustedHeight) {
 
 void aboutProgram(int width, int height) {
 	clearScreen();
-	moveCursor((width - 45) / 2, height / 4);
+	moveCursor((currTerminalWidth - 45) / 2, height / 4);
 	printf("RSA Cipher Tool with Custom Bignum Library");
 
 	char *about[] = {"The RSA Cipher Tool program generates, encrypts, and decrypts messages using the RSA", "algorithm. The program uses the RSA (Rivest, Shamir, Adleman) Algorithm to generate public", "and private keys needed to encrypt and decrypt messages and a custom bignum library to", "handle large numbers numbers for the RSA encryption and decryption processes."};
 	int aboutCount = sizeof(about) / sizeof(about[0]);
-	displayAbout(about, aboutCount, width, height, 2);
+	displayAbout(about, aboutCount, currTerminalWidth, height, 2);
 
 	char* about2[] = {"The user will use the program to encrypt a message in a file, where it will then return the", "encrypted/secret message. Then, they can now send it to the recipient of the message safely,", "where they can also use the same program to decrypt the message and reveal the actual message."};
 	int about2Count = sizeof(about2) / sizeof(about2[0]);
-	displayAbout(about2, about2Count, width, height, 7);
+	displayAbout(about2, about2Count, currTerminalWidth, height, 7);
 
 	char* about3[] = {"This is a programming project for Computer Programming 2 during the Academic Year", "2023-2024 at the Bicol University College of Science. The program is developed by the", "following students of Bicol University College of Science:"};
 	int about3Count = sizeof(about3) / sizeof(about3[0]);
-	displayAbout(about3, about3Count, width, height, 11);
+	displayAbout(about3, about3Count, currTerminalWidth, height, 11);
 
 	char* members[] = { "Michael Xavier Canonizado", "Deanne Clarice Bea", "Simon Narvaez", "Marc Jordan Campopos"};
 	int membersCount = sizeof(members) / sizeof(members[0]);
-	displayAbout(members, membersCount, width, height, 14);
+	displayAbout(members, membersCount, currTerminalWidth, height, 14);
 
-	waitForDone(width, height);
+	waitForDone(currTerminalWidth, height);
 	clearScreen();
 }
